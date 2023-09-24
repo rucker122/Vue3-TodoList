a545fde0117593bac475e6135a9997fb76307f93
rucker122 <rucker122@gmail.com>
2023-09-24 15:31:34 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <h1 class="green">Task Tool</h1>
+    <h3>
+      Adding whatever you want! 
+    </h3>
+    <input type="text" v-model="taskTitle" v-on:keyup.enter="addList()" />
+    <ul>
+      <ol v-for="item in getList()">
+        {{ item }}
+      </ol>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+  interface Model{
+    todoList: string[]
+  }
+  export default defineComponent({
+    data() {
+      return {
+        todoList: <string[]>[],
+        taskTitle: <string>''
+      }
+    },
+    methods:{
+      getList() {
+        return this.todoList;
+      },
+      addList(): void {
+        console.log('123');
+        if (this.taskTitle != '') {
+          this.todoList.push(this.taskTitle);
+          this.taskTitle = '';
+        }
+      }
+    },
+    mounted() {
+
+    }
+  });
+</script>
